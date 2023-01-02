664a036c00fbc0ca9cf8a9e4d15ef146c2ddfa9a
Anderson Hsieh <andy13579246810@gmail.com>
2023-01-02 04:45:16 -0500

    add usb permission stuff

--- app/src/main/java/com/anderson_hsieh/uart_usb_connection/MainActivity.kt
+++ app/src/main/java/com/anderson_hsieh/uart_usb_connection/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.anderson_hsieh.uart_usb_connection
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.hardware.usb.UsbDevice
+import android.hardware.usb.UsbManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
+private const val ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION"
+
+private val usbReceiver = object : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        if (ACTION_USB_PERMISSION == intent.action) {
+            // like a semaphore for this broadcast receiver, haha
+            synchronized(this) {
+                val device: UsbDevice? = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE)
+
+                if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+                    device?.apply {
+                        //call method to set up device communication
+
+                    }
+                } else {
+                    Log.d("permission", "permission denied for device $device")
+                }
+            }
+        }
+    }
+}
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val device: UsbDevice? = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE)
+        
     }
 }
