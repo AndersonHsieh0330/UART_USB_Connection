2ff87c7e0178e6f5639aa577c05b2b1967279948
Anderson Hsieh <andy13579246810@gmail.com>
2023-01-03 00:40:53 -0500

    implement recyclerview and load all device names when try to connect

--- app/src/main/java/com/anderson_hsieh/uart_usb_connection/MainActivity.kt
+++ app/src/main/java/com/anderson_hsieh/uart_usb_connection/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.anderson_hsieh.uart_usb_connection
 
+import android.app.AlertDialog
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.hardware.usb.UsbDevice
 import android.hardware.usb.UsbManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.button.MaterialButton
+import android.content.DialogInterface
+
+
+
 
 private const val ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION"
 
@@ -34,11 +44,57 @@ private val usbReceiver = object : BroadcastReceiver() {
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var dataSet: ArrayList<String>
+    lateinit var recyclerView: RecyclerView
+    lateinit var usbManager: UsbManager
+    lateinit var connectBtn: MaterialButton
+    lateinit var alertDialog: AlertDialog
+    var device: UsbDevice? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val device: UsbDevice? = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE)
-        
+        dataSet = ArrayList()
+        dataSet.add("1")
+        dataSet.add("2")
+        dataSet.add("3")
+        recyclerView = findViewById(R.id.serial_recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = SerialRecyclerAdapter(dataSet)
+
+        usbManager = getSystemService(Context.USB_SERVICE) as UsbManager
+        val filter = IntentFilter(ACTION_USB_PERMISSION)
+        registerReceiver(usbReceiver, filter)
+
+        initAlertDialog();
+        initConntBtn(PendingIntent.getBroadcast(this, 0, Intent(ACTION_USB_PERMISSION), 0));
+    }
+
+    fun initAlertDialog(){
+        alertDialog = AlertDialog.Builder(this@MainActivity).create()
+        alertDialog.setTitle(getString(R.string.oops))
+        alertDialog.setMessage(getString(R.string.no_device))
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, getString(R.string.ok),
+            DialogInterface.OnClickListener { dialog, _ -> dialog.dismiss() })
+    }
+
+    fun initConntBtn(permissionIntent:PendingIntent) {
+        connectBtn = findViewById(R.id.connect_btn);
+        connectBtn.setOnClickListener {
+            val deviceList = usbManager.deviceList
+            deviceList.forEach {
+                dataSet.add(it.value.deviceName);
+                recyclerView.adapter?.apply {
+                    notifyDataSetChanged()
+                }
+            }
+            device = deviceList["deviceName"]
+            if (device != null) {
+                usbManager.requestPermission(device, permissionIntent)
+            } else {
+                alertDialog.show()
+            }
+        }
     }
 }
